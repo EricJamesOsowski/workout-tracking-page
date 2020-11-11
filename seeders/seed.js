08c85f3a9f474770e0403267722cbecd9aec0d03
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-11-11 09:28:42 -0600

    HTML routing setup

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,4 +1,4 @@
-let mongoose = require("mongoose");
+ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect("mongodb://localhost/workout", {
---
+++ server.js
@@ -0,0 +1,167 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Setting up Express App
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Mongo boilerplate 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Required routing
+// require("./routes/apiRoutes")(app);
+require("./routes/html-routes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
+});
+
+
+// const express = require("express");
+// const mongojs = require("mongojs");
+// const logger = require("morgan");
+// const path = require("path");
+
+// const app = express();
+
+// app.use(logger("dev"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// const databaseUrl = "workoutsDB";
+// const collections = ["workouts"];
+
+// const db = mongojs(databaseUrl, collections);
+
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + "./public/index.html"));
+// });
+
+// app.post("/submit", (req, res) => {
+//   console.log(req.body);
+
+//   db.workouts.insert(req.body, (error, data) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
+
+// app.get("/exercise", (req, res) => {
+//   db.workouts.find({}, (error, data) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
+
+// app.get("/api/workouts/:id", (req, res) => {
+//   db.workouts.findOne(
+//     {
+//       _id: mongojs.ObjectId(req.params.id)
+//     },
+//     (error, data) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(data);
+//       }
+//     }
+//   );
+// });
+
+// app.post("/update/:id", (req, res) => {
+//   db.workouts.update(
+//     {
+//       _id: mongojs.ObjectId(req.params.id)
+//     },
+//     {
+//       $set: {
+//         title: req.body.title,
+//         note: req.body.note,
+//         modified: Date.now()
+//       }
+//     },
+//     (error, data) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(data);
+//       }
+//     }
+//   );
+// });
+
+// app.post("/api/workouts/:id", (req, res) => {
+//     db.workouts.update(
+//       {
+//         _id: mongojs.ObjectId(req.params.id)
+//       },
+//       {
+//         $set: {
+//           title: req.body.title,
+//           note: req.body.note,
+//         }
+//       },
+//       (error, data) => {
+//         if (error) {
+//           res.send(error);
+//         } else {
+//           res.send(data);
+//         }
+//       }
+//     );
+//   });
+
+// app.delete("/delete/:id", (req, res) => {
+//   db.workouts.remove(
+//     {
+//       _id: mongojs.ObjectID(req.params.id)
+//     },
+//     (error, data) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(data);
+//       }
+//     }
+//   );
+// });
+
+// app.delete("/clearall", (req, res) => {
+//   db.workouts.remove({}, (error, response) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.send(response);
+//     }
+//   });
+// });
+
+// app.listen(3000, () => {
+//   console.log("App running on port 3000!");
+// });
