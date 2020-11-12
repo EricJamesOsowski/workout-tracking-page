8dc3231bd9862655c9f9c767da05102366a547e3
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-11-11 22:31:04 -0600

    code refactoring

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,37 +5,38 @@ module.exports = function (app) {
 
   app.get("/api/workouts", (req, res) => {
     Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    }).catch((err) => {
-      res.json(err);
-    });
+      .then((data) => {
+        res.json(data);
+      }).catch((err) => {
+        res.json(err);
+      });
   });
 
   app.post("/api/workouts", function (req, res) {
     Workout.create({})
-        .then(data => res.json(data))
-        .catch(err => {
-            console.log("err", err)
-            res.json(err)
-        })
-});
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log("err", err)
+        res.json(err)
+      })
+  });
 
   app.put("/api/workouts/:id", (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { useFindAndModify: false })
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    }).catch((err) => {
-      res.json(err);
-    });
+      .then((data) => {
+        res.json(data);
+      }).catch((err) => {
+        res.json(err);
+      });
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    Workout.find({}).sort({ day: -1 }).limit(7).then(dbWorkout => {
-      res.json(dbWorkout)
-    }).catch((err) => {
-      res.json(err);
-    });
+    Workout.find({}).sort({ day: -1 }).limit(7)
+      .then(data => {
+        res.json(data)
+      }).catch((err) => {
+        res.json(err);
+      });
   });
 
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,6 @@
 const path = require("path");
 
 module.exports = function (app){
-    
     app.get("/exercise", function (req,res){
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
@@ -10,9 +9,8 @@ module.exports = function (app){
     app.get("/", function(req,res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
-
+    
     app.get("/stats", function(req, res){
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
-
 }
